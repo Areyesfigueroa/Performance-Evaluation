3659df295571f8869752d21f928b376ca6b544bb
Aliel Reyes <alielreyes@gmail.com>
2019-07-19 12:29:44 -0700

    Adding db_connection file
    - Adding Db Connection file so that I can work on it on a mac laptop as well.

---
+++ includes/db_connection.php
@@ -0,0 +1,19 @@
+<?php
+
+function OpenCon()
+{
+    $dbhost = "localhost";
+    $dbuser = "root";
+    $dbpass = "*^59MVqs#1c8";
+    $db = "mydatabase";
+
+    $conn = new mysqli($dbhost, $dbuser, $dbpass, $db) or die("Connect failed: %s\n". $conn ->error);
+
+    return $conn;
+}
+
+function CloseCon($conn){
+    $conn -> close();
+}
+
+?>
