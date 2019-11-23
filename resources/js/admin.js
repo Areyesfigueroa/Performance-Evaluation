aaab6761e0d50742c495f415ec1775b631a7866c
Aliel Reyes <alielreyes@gmail.com>
2019-11-23 13:52:30 -0800

    Checkbox Functionality Fixed
    Added new way of storing and removing checkboxes.

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -12,20 +12,25 @@ const adminModelController = (function() {
         ['Aliel Reyes', 'areyes@allaboutparking.com', 'admin'],
         ['Aliel Reyes', 'areyes@allaboutparking.com', 'admin'],
         ['Aliel Reyes', 'areyes@allaboutparking.com', 'admin'],
+        ['Aliel Reyes', 'areyes@allaboutparking.com', 'admin'],
+        ['Aliel Reyes', 'areyes@allaboutparking.com', 'admin'],
+        ['Aliel Reyes', 'areyes@allaboutparking.com', 'admin'],
+        ['Aliel Reyes', 'areyes@allaboutparking.com', 'admin'],
+        ['Aliel Reyes', 'areyes@allaboutparking.com', 'admin'],
         ['Aliel Reyes', 'areyes@allaboutparking.com', 'admin']
     ];
 
     //TODO: Work out a data structure store the index values for the checkboxes that are on, so that we can use it later and quickly know which data we are referring to. 
-    const checkedRows = []
+    let checkedRows = []; //FOR CHECKLISTS INSTEAD OF NUMBERS USE REFERENCES OBJ
 
-    const resetPwd = (id) => {
-        console.log(`Reset Pwd: ${id}`);
+    const resetPwd = (tableRow) => {
+        console.log(`Reset Pwd: ${tableRow.id}`);
     }
-    const removeUser = (id) => {
-        console.log(`Remove User: ${id}`);
+    const removeUser = (tableRow) => {
+        console.log(`Remove User: ${tableRow.id}`);
     }
-    const changeRole = (id) => {
-        console.log(`Change Role: ${id}`);
+    const changeRole = (tableRow) => {
+        console.log(`Change Role: ${tableRow.id}`);
     }
 
 
@@ -43,9 +48,30 @@ const adminModelController = (function() {
             }
         },
 
-        rowChecked: (id, value) => {
-            console.log(`Row Checked - ${id}: Value - ${value}`);
-            //Save the true values and remove the false ones. 
+        getCheckedRows: () => {
+            return checkedRows;
+        },
+
+        rowChecked: (checkbox) => {            
+
+            //Save the true values and remove the false one
+            if(checkbox.checked) {
+                //Record the checkbox object
+                checkedRows.push(checkbox);
+            } else {
+                //Remove rowIdx
+                const rowIdx = checkedRows.findIndex(el => el.id === checkbox.id);
+               
+                if(rowIdx >= 0) {
+                    checkedRows.splice(rowIdx, 1);
+                }
+            }
+            console.log(checkedRows);
+        }, 
+
+        clearCheckedRows: () => {
+            checkedRows = [];
+            console.log(checkedRows);
         }
     }
 
@@ -63,28 +89,16 @@ const adminUIController = (function() {
         changeRoleBtn: "change-role-btn-"
     }
 
-    const setCheckbox = (active, idNum, checkbox = DOMstrings.checkbox) => {
-        document.getElementById(`${checkbox}${idNum}`).checked = active;
-    }
-
     return {
 
         getDOMstrings: () => {
             return DOMstrings;
         },
 
-        setAllCheckboxes: (active) => {
-            const adminTable = document.getElementById(DOMstrings.adminTable).getElementsByTagName(DOMstrings.tableBody)[0];
-            adminTableArr = Array.prototype.slice.call(adminTable.rows, 0);
-            
-            //Set table Header
-            setCheckbox(active, 0, DOMstrings.selectAllCheckbox);
-            setCheckbox(active, 1, DOMstrings.selectAllCheckbox);
-            
-            //Set table Body
-            adminTableArr.forEach((e, i) => {
-                setCheckbox(active, i);
-            });            
+        setCheckbox: (active, idNum, checkbox = DOMstrings.checkbox) => {
+            if( document.getElementById(`${checkbox}${idNum}`)) {
+                document.getElementById(`${checkbox}${idNum}`).checked = active;
+            }
         },
 
         createCheckbox: (parentElement, id) => {
@@ -127,41 +141,67 @@ const adminController = (function(aModelCtrl, aUICtrl) {
         DOMstrings = aUICtrl.getDOMstrings();
         const selectAllLength = 2;
 
-        
         //Select All Checkbox Event Listeners
         for(let i =0; i < selectAllLength; i++) {
             document.getElementById(`${DOMstrings.selectAllCheckbox}${i}`).addEventListener('change', function() {
-                
                 let status = document.getElementById(`${DOMstrings.selectAllCheckbox}${i}`).checked;
-                aUICtrl.setAllCheckboxes(status);
+                updateAllCheckboxes(status);
             });
         }
 
-        //Set checkbox event listeners
-        const setCheckboxBtn = (i, btnID, callback) => {
+        //Set list based listeners
+        const setEventListener = (i, btnID, callback) => {
             document.getElementById(`${btnID}${i}`).addEventListener('click', (event) => {
-                callback(event.target.id, event.target.checked);
+                callback(event.target);
             });
         }
 
-        
-        //Set adming actions event listeners
-        const setActionButton = (i, btnID, callback) => {
-            document.getElementById(`${btnID}${i}`).addEventListener('click', (event) => {
-                callback(event.target.id);
-            });
-        }
 
         aModelCtrl.getData().forEach((_, i) => {
-            setActionButton(i, DOMstrings.resetPwdBtn, aModelCtrl.getActions().resetPwd);
-            setActionButton(i, DOMstrings.removeUserBtn, aModelCtrl.getActions().removeUser);
-            setActionButton(i, DOMstrings.changeRoleBtn, aModelCtrl.getActions().changeRole);
-            setCheckboxBtn(i, DOMstrings.checkbox, aModelCtrl.rowChecked);
+            setEventListener(i, DOMstrings.resetPwdBtn, aModelCtrl.getActions().resetPwd);
+            setEventListener(i, DOMstrings.removeUserBtn, aModelCtrl.getActions().removeUser);
+            setEventListener(i, DOMstrings.changeRoleBtn, aModelCtrl.getActions().changeRole);
+            setEventListener(i, DOMstrings.checkbox, aModelCtrl.rowChecked);
         });
-
     }
 
+    const updateAllCheckboxes = (value) => {
+        const DOMstrings = aUICtrl.getDOMstrings();
+        
+        const adminTable = document.getElementById(DOMstrings.adminTable).getElementsByTagName(DOMstrings.tableBody)[0];
+        adminTableArr = Array.prototype.slice.call(adminTable.rows, 0);
+        
+        if(value) {
+            //Check box header checkboxes
+            aUICtrl.setCheckbox(value, 0, DOMstrings.selectAllCheckbox);
+            aUICtrl.setCheckbox(value, 1, DOMstrings.selectAllCheckbox);  
 
+            //Check all visible checkboxes. 
+            adminTableArr.forEach((el) => {
+                //Update UI
+                const id = parseInt(el.id.replace("row-", ""));
+                aUICtrl.setCheckbox(true, id);
+                
+                //Update Data
+                aModelCtrl.rowChecked(document.getElementById(`${DOMstrings.checkbox}${id}`));
+            });      
+
+        } else {
+
+            //Check box header checkboxes
+            aUICtrl.setCheckbox(value, 0, DOMstrings.selectAllCheckbox);
+            aUICtrl.setCheckbox(value, 1, DOMstrings.selectAllCheckbox); 
+
+            //Uncheck all stored checkboxes.
+            aModelCtrl.getCheckedRows().forEach((el) => {
+                //Update UI
+                el.checked = false;
+            }); 
+
+            //Update Data
+            aModelCtrl.clearCheckedRows();
+        }
+    }
 
     const createAdminTable = () => {
 
