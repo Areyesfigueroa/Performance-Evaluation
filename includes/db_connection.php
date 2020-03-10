bb0dac768640df647d691c85595cfc411baafc16
Aliel Reyes <alielreyes@gmail.com>
2020-03-09 18:22:33 -0700

    Testing new db

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -2,10 +2,10 @@
 
 function OpenCon()
 {
-    $dbhost = "localhost";
-    $dbuser = "root";
-    $dbpass = "tUtYmttgIapLF8CJ"; // *^59MVqs#1c8, Workbench DB Password, XAMPP = "".
-    $db = "performance_evaluation";
+    $dbhost = "us-cdbr-iron-east-04.cleardb.net";
+    $dbuser = "b7a21ff66261c4";
+    $dbpass = "9fd05532"; // *^59MVqs#1c8, Workbench DB Password, XAMPP = "".
+    $db = "heroku_47446b38f6e78f3";
 
     $conn = new mysqli($dbhost, $dbuser, $dbpass, $db) or die("Connect failed: %s\n". $conn ->error);
 
