c990275ceb732aa97956f2c60afafacc4f1dfbd8
Aliel Reyes <alielreyes@gmail.com>
2020-03-10 21:53:56 -0700

    Testing vendor packages

--- includes/footer.php
+++ includes/footer.php
@@ -14,13 +14,17 @@
             <!---------------------->
 
             <!-- JQuery & Popper & Bootstrap -->
-            <script type="text/javascript" src="vendor/mdb/js/jquery-3.4.1.min.js"></script>
-            <script type="text/javascript" src="vendor/mdb/js/popper.min.js"></script>
-            <script type="text/javascript" src="vendor/mdb/js/bootstrap.min.js"></script>
-
+            <!-- JQuery -->
+            <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+            <!-- Bootstrap tooltips -->
+            <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+            <!-- Bootstrap core JavaScript -->
+            <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.min.js"></script>
+            <!-- MDB core JavaScript -->
+            <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.14.0/js/mdb.min.js"></script>
 
             <!-- MDBootstrap Datatables -->
-            <script type="text/javascript" src="vendor/mdb/js/addons/datatables.min.js"></script>
+            <script type="text/javascript" src="myvendors/mdb/js/addons/datatables.min.js"></script>
 
             <!---------------------->
             <!--- CUSTOM SCRIPTS --->
